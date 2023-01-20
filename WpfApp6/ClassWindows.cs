c01b9a2c13ad535063283c9e4a247ce4785eecda
Ксения Гришанина <grishanina0404@mail.ru>
2023-01-20 17:46:44 +0300

    Добавьте файлы проекта.

---
+++ WpfApp6/ClassWindows.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+
+namespace WpfApp6
+{
+    public static class ClassWindows
+    {
+        // блок с данными
+        static int count = 0;
+        static int count1 = 0;
+        public static List<string> datalist = new List<string> { "Сложение", "Вычитание", "Деление", "Умножение" };
+
+        // блок для обращения к текстовым полям
+        public static TextBox tb1;
+        public static TextBox tb2;
+        public static TextBlock tbCombo;
+
+        // блок с бизнес-логикой
+        public static int Count_1 
+        {
+            get
+            {
+                return count;
+            }
+            set
+            {
+                count = value;
+                tb1.Text = count.ToString();
+            }
+        }
+
+        public static int Count_2 // свойство для отображения числа в TextBlock
+        {
+            get
+            {
+                return count1;
+            }
+            set
+            {
+                count1 = value;
+                tb2.Text = count1.ToString();
+            }
+        }
+
+
+        public static int Combo
+        {
+            set
+            {
+                if (datalist[value] == "Сложение")
+                {
+                    tbCombo.Text = "+";
+                }
+                else if (datalist[value] == "Вычитание")
+                {
+                    tbCombo.Text = "-";
+                }
+                else if (datalist[value] == "Деление")
+                {
+                    tbCombo.Text = "/";
+                }
+                else if (datalist[value] == "Умножение")
+                {
+                    tbCombo.Text = "*";
+                }
+                //tbCombo.Text = datalist[value];
+            }
+        }
+    }
+}
---
+++ WpfApp6/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp6
+{
+    /// <summary>
+    /// Логика взаимодействия для Window1.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            ClassWindows.tb1 = First;
+            //First = ClassWindows.tb1;
+            ClassWindows.tbCombo = Znach;
+            //Second = ClassWindows.tb2;
+            ClassWindows.tb2 = Second;
+            Box.ItemsSource = ClassWindows.datalist;
+        }
+
+        private void BtnCount_Click(object sender, RoutedEventArgs e)
+        {
+            ClassWindows.Count_1 = Convert.ToInt32(First.Text);
+            ClassWindows.Count_2 = Convert.ToInt32(Second.Text);
+
+            if(Znach.Text == "+")
+            {
+                double Result_0 = (double)ClassWindows.Count_1 + (double)ClassWindows.Count_2;
+                Result.Text = Result_0.ToString();
+            }
+            else if(Znach.Text == "-")
+            {
+                double Result_0 = (double)ClassWindows.Count_1 - (double)ClassWindows.Count_2;
+                Result.Text = Result_0.ToString();
+            }
+            else if (Znach.Text == "*")
+            {
+                double Result_0 = (double)ClassWindows.Count_1 * (double)ClassWindows.Count_2;
+                Result.Text = Result_0.ToString();
+            }
+            else if (Znach.Text == "/")
+            {
+                double Result_0 = (double)ClassWindows.Count_1 / (double)ClassWindows.Count_2;
+                Result.Text = Result_0.ToString();
+            }
+        }
+
+        private void Box_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ClassWindows.Combo = Box.SelectedIndex;
+        }
+    }
+}
